e01c20d9e658c505b9c58baead48189809bf8571
BlowTorchBruh <allenwilson000@gmail.com>
2025-06-08 15:57:03 -0500

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    fetchCasePrice: (caseName) => ipcRenderer.invoke('fetch-case-price', caseName),
-    fetchAllCasePrices: (caseNames) => ipcRenderer.invoke('fetch-all-case-prices', caseNames)
-});
